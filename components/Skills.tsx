a99b91f8c18d55e020da990c03716f83ff0b3369
Amit Kushwaha <amitcoding56@gmail.com>
2025-07-01 12:49:03 +0530

    Error fix

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 function Skills() {
   const skills = [
@@ -17,13 +17,12 @@ function Skills() {
     "Git",
     "Vercel",
     "Github",
-    
   ];
 
   return (
     <div className="mt-12 space-y-4">
       <h2 className="text-white text-xl">Skills</h2>
-      <div className="flex flex-wrap space-x-2 space-y-2">
+      <div className="flex flex-wrap gap-2">
         {skills.map((skill, index) => (
           <span
             className="text-gray-300 text-sm border bg-[#1e2a3a] py-1 px-3 rounded-md border-gray-400"
